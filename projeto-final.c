3d7d9b3e528582596bed41eaa137f57f5e45cf97
Guilherme-07062002 <guilherme762002@gmail.com>
2025-02-11 17:31:51 -0300

    feat: enhance LED response based on sound intensity with improved color mapping

--- projeto-final.c
+++ projeto-final.c
@@ -470,22 +470,69 @@ int main() {
 
             // A depender da intensidade do som, acende LEDs específicos com cores diferentes
             switch (intensity) {
-                case 0: 
-                    printf("Intensidade muito baixa\n");
-                    npSetLED(12, 255, 0, 0); // Vermelho
-                    break; // Se o som for muito baixo, não acende nada.
+                case 0: break; // Se o som for muito baixo, não acende nada.
                 case 1:
-                    npSetLED(12, 255, 0, 0); // Vermelho
-                    break;
+                npSetLED(12, 0, 0, 80); // Acende apenas o centro.
+                break;
                 case 2:
-                    npSetLED(12, 0, 255, 0); // Verde
-                    break;
+                npSetLED(12, 0, 0, 120); // Acente o centro.
+        
+                // Primeiro anel.
+                npSetLED(7, 0, 0, 80);
+                npSetLED(11, 0, 0, 80);
+                npSetLED(13, 0, 0, 80);
+                npSetLED(17, 0, 0, 80);
+                break;
                 case 3:
-                    npSetLED(12, 0, 0, 255); // Azul
-                    break;
+                // Centro.
+                npSetLED(12, 60, 60, 0);
+        
+                // Primeiro anel.
+                npSetLED(7, 0, 0, 120);
+                npSetLED(11, 0, 0, 120);
+                npSetLED(13, 0, 0, 120);
+                npSetLED(17, 0, 0, 120);
+        
+                // Segundo anel.
+                npSetLED(2, 0, 0, 80);
+                npSetLED(6, 0, 0, 80);
+                npSetLED(8, 0, 0, 80);
+                npSetLED(10, 0, 0, 80);
+                npSetLED(14, 0, 0, 80);
+                npSetLED(16, 0, 0, 80);
+                npSetLED(18, 0, 0, 80);
+                npSetLED(22, 0, 0, 80);
+                break;
                 case 4:
-                    npSetLED(12, 255, 255, 0); // Amarelo
-                    break;
+                // Centro.
+                npSetLED(12, 80, 0, 0);
+        
+                // Primeiro anel.
+                npSetLED(7, 60, 60, 0);
+                npSetLED(11, 60, 60, 0);
+                npSetLED(13, 60, 60, 0);
+                npSetLED(17, 60, 60, 0);
+        
+                // Segundo anel.
+                npSetLED(2, 0, 0, 120);
+                npSetLED(6, 0, 0, 120);
+                npSetLED(8, 0, 0, 120);
+                npSetLED(10, 0, 0, 120);
+                npSetLED(14, 0, 0, 120);
+                npSetLED(16, 0, 0, 120);
+                npSetLED(18, 0, 0, 120);
+                npSetLED(22, 0, 0, 120);
+        
+                // Terceiro anel.
+                npSetLED(1, 0, 0, 80);
+                npSetLED(3, 0, 0, 80);
+                npSetLED(5, 0, 0, 80);
+                npSetLED(9, 0, 0, 80);
+                npSetLED(15, 0, 0, 80);
+                npSetLED(19, 0, 0, 80);
+                npSetLED(21, 0, 0, 80);
+                npSetLED(23, 0, 0, 80);
+                break;
             }
             // Atualiza a matriz.
             npWrite();
