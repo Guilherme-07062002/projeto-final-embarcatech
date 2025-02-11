4f4ffd87f03a66da44e1ab5ef470489d2555b8e9
Guilherme-07062002 <guilherme762002@gmail.com>
2025-02-11 12:07:33 -0300

    feat: enhance microphone sampling and intensity calculation with improved logging and LED feedback

--- projeto-final.c
+++ projeto-final.c
@@ -272,49 +272,53 @@ uint8_t get_intensity(float v);
  * Realiza as leituras do ADC e armazena os valores no buffer.
  */
 void sample_mic() {
-  adc_fifo_drain(); // Limpa o FIFO do ADC.
-  adc_run(false); // Desliga o ADC (se estiver ligado) para configurar o DMA.
+    // Force o ADC a ler do canal do microfone
+    adc_select_input(MIC_CHANNEL);
 
-  dma_channel_configure(dma_channel, &dma_cfg,
-    adc_buffer, // Escreve no buffer.
-    &(adc_hw->fifo), // Lê do ADC.
-    SAMPLES, // Faz SAMPLES amostras.
-    true // Liga o DMA.
-  );
+    adc_fifo_drain(); // Limpa o FIFO do ADC.
+    adc_run(false);   // Desliga o ADC para configurar o DMA.
 
-  // Liga o ADC e espera acabar a leitura.
-  adc_run(true);
-  dma_channel_wait_for_finish_blocking(dma_channel);
+    dma_channel_configure(dma_channel, &dma_cfg,
+        adc_buffer,         // Escreve no buffer.
+        &(adc_hw->fifo),    // Lê do ADC.
+        SAMPLES,            // Faz SAMPLES amostras.
+        true                // Liga o DMA.
+    );
+
+    // Liga o ADC e espera acabar a leitura.
+    adc_run(true);
+    dma_channel_wait_for_finish_blocking(dma_channel);
   
-  // Acabou a leitura, desliga o ADC de novo.
-  adc_run(false);
+    // Acabou a leitura, desliga o ADC de novo.
+    adc_run(false);
 }
 
 /**
  * Calcula a potência média das leituras do ADC. (Valor RMS)
  */
 float mic_power() {
-  float avg = 0.f;
-
-  for (uint i = 0; i < SAMPLES; ++i)
-    avg += adc_buffer[i] * adc_buffer[i];
+    float avg = 0.f;
   
-  avg /= SAMPLES;
-  return sqrt(avg);
-}
-
-/**
- * Calcula a intensidade do volume registrado no microfone, de 0 a 4, usando a tensão.
- */
-uint8_t get_intensity(float v) {
-  uint count = 0;
-
-  while ((v -= ADC_STEP/20) > 0.f)
-    ++count;
+    for (uint i = 0; i < SAMPLES; ++i)
+      avg += adc_buffer[i] * adc_buffer[i];
+    
+    avg /= SAMPLES;
+    return sqrt(avg);
+  }
   
-  return count;
-}
-
+  /**
+   * Calcula a intensidade do volume registrado no microfone, de 0 a 4, usando a tensão.
+   */
+  uint8_t get_intensity(float v) {
+        printf("Tensão: %f\n", v);
+        
+        uint count = 0;
+        
+        while ((v -= ADC_STEP/20) > 0.f)
+          ++count;
+        
+        return count;
+    }
 
 /**
  * Main.
@@ -361,12 +365,14 @@ int main() {
     // send_http_request();
 
     // Preparação da matriz de LEDs.
-    printf("Preparando NeoPixel...");
+    printf("Preparando NeoPixel...\n");
     npInit(LED_PIN, LED_COUNT);
 
     // Preparação do ADC.
     printf("Preparando ADC...\n");
     adc_gpio_init(MIC_PIN);
+    adc_init();
+
     adc_select_input(MIC_CHANNEL);
     adc_fifo_setup(
         true, // Habilitar FIFO
@@ -378,20 +384,23 @@ int main() {
     adc_set_clkdiv(ADC_CLOCK_DIV);
     printf("ADC Configurado!\n\n");
 
-    printf("Preparando DMA...");
+    printf("Preparando DMA...\n");
     // Tomando posse de canal do DMA.
     dma_channel = dma_claim_unused_channel(true);
+    
     // Configurações do DMA.
     dma_cfg = dma_channel_get_default_config(dma_channel);
+    
     channel_config_set_transfer_data_size(&dma_cfg, DMA_SIZE_16); // Tamanho da transferência é 16-bits (usamos uint16_t para armazenar valores do ADC)
     channel_config_set_read_increment(&dma_cfg, false); // Desabilita incremento do ponteiro de leitura (lemos de um único registrador)
     channel_config_set_write_increment(&dma_cfg, true); // Habilita incremento do ponteiro de escrita (escrevemos em um array/buffer)
+    
     channel_config_set_dreq(&dma_cfg, DREQ_ADC); // Usamos a requisição de dados do ADC
 
     // Amostragem de teste.
     printf("Amostragem de teste...\n");
     sample_mic();
-    printf("Configuracoes completas!\n");
+    printf("Configurações completas!\n");
 
     printf("\n----\nIniciando loop...\n----\n");
 
@@ -413,83 +422,49 @@ int main() {
 
         // Verifica se o botão está pressionado
         while (gpio_get(BUTTON_A) == 0) {
-            // Coleta de áudio
+                    
+            // Realiza uma amostragem do microfone.
             sample_mic();
+
+            // Pega a potência média da amostragem do microfone.
             float avg = mic_power();
             avg = 2.f * abs(ADC_ADJUST(avg)); // Ajusta para intervalo de 0 a 3.3V. (apenas magnitude, sem sinal)
-            uint intensity = get_intensity(avg); // Calcula intensidade a ser mostrada na matriz de LEDs
 
-            // Limpa a matriz de LEDs
+            uint intensity = get_intensity(avg); // Calcula intensidade a ser mostrada na matriz de LEDs.
+
+            // Limpa a matriz de LEDs.
             npClear();
 
-            // A depender da intensidade do som, acende LEDs específicos
+            // A depender da intensidade do som, acende LEDs específicos com cores diferentes
             switch (intensity) {
-                case 0: break; // Se o som for muito baixo, não acende nada.
+                case 0: 
+                    printf("Intensidade muito baixa\n");
+                    npSetLED(12, 255, 0, 0); // Vermelho
+                    break; // Se o som for muito baixo, não acende nada.
                 case 1:
-                    npSetLED(12, 0, 0, 80); // Acende apenas o centro.
+                    npSetLED(12, 255, 0, 0); // Vermelho
                     break;
                 case 2:
-                    npSetLED(12, 0, 0, 120); // Acende o centro.
-                    // Primeiro anel.
-                    npSetLED(7, 0, 0, 80);
-                    npSetLED(11, 0, 0, 80);
-                    npSetLED(13, 0, 0, 80);
-                    npSetLED(17, 0, 0, 80);
+                    npSetLED(12, 0, 255, 0); // Verde
                     break;
                 case 3:
-                    // Centro.
-                    npSetLED(12, 60, 60, 0);
-                    // Primeiro anel.
-                    npSetLED(7, 0, 0, 120);
-                    npSetLED(11, 0, 0, 120);
-                    npSetLED(13, 0, 0, 120);
-                    npSetLED(17, 0, 0, 120);
-                    // Segundo anel.
-                    npSetLED(2, 0, 0, 80);
-                    npSetLED(6, 0, 0, 80);
-                    npSetLED(8, 0, 0, 80);
-                    npSetLED(10, 0, 0, 80);
-                    npSetLED(14, 0, 0, 80);
-                    npSetLED(16, 0, 0, 80);
-                    npSetLED(18, 0, 0, 80);
-                    npSetLED(22, 0, 0, 80);
+                    npSetLED(12, 0, 0, 255); // Azul
                     break;
                 case 4:
-                    // Centro.
-                    npSetLED(12, 80, 0, 0);
-                    // Primeiro anel.
-                    npSetLED(7, 60, 60, 0);
-                    npSetLED(11, 60, 60, 0);
-                    npSetLED(13, 60, 60, 0);
-                    npSetLED(17, 60, 60, 0);
-                    // Segundo anel.
-                    npSetLED(2, 0, 0, 120);
-                    npSetLED(6, 0, 0, 120);
-                    npSetLED(8, 0, 0, 120);
-                    npSetLED(10, 0, 0, 120);
-                    npSetLED(14, 0, 0, 120);
-                    npSetLED(16, 0, 0, 120);
-                    npSetLED(18, 0, 0, 120);
-                    npSetLED(22, 0, 0, 120);
-                    // Terceiro anel.
-                    npSetLED(1, 0, 0, 80);
-                    npSetLED(3, 0, 0, 80);
-                    npSetLED(5, 0, 0, 80);
-                    npSetLED(9, 0, 0, 80);
-                    npSetLED(15, 0, 0, 80);
-                    npSetLED(19, 0, 0, 80);
-                    npSetLED(21, 0, 0, 80);
-                    npSetLED(23, 0, 0, 80);
+                    npSetLED(12, 255, 255, 0); // Amarelo
                     break;
             }
             // Atualiza a matriz.
             npWrite();
 
-            // Envia a intensidade e a média das leituras do ADC por serial.
-            printf("%2d %8.4f\r", intensity, avg);
+            // Exibe intensidade do som no terminal.
+            printf("Intensidade: %d\n", intensity);
+        }
 
-            // Pequena pausa para evitar leitura contínua muito rápida
-            sleep_ms(50);
+        // Se botão não está pressionado limpa a matriz de LEDs.
+        if (gpio_get(BUTTON_A) == 1) {
+            npClear();
+            npWrite();
         }
 
         // Atualiza a rolagem do texto
